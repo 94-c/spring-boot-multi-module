c0857b0d7b44702648d53fc7fb892bc35d83a1f5
94-c <hyeongwoo26@gmail.com>
2024-10-16 01:49:05 +0900

    [feat] 멀티 모듈화로 인한 어플리케이션

--- blog-api/src/main/java/com/backend/api/BlogApiApplication.java
+++ blog-api/src/main/java/com/backend/api/BlogApiApplication.java
@@ -2,8 +2,13 @@ package com.backend.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class})
+@ComponentScan(basePackages = {"com.backend.api", "com.backend.core"})
 public class BlogApiApplication {
 
     public static void main(String[] args) {
--- blog-core/src/main/java/com/backend/core/BlogCoreApplication.java
+++ blog-core/src/main/java/com/backend/core/BlogCoreApplication.java
@@ -2,8 +2,10 @@ package com.backend.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.backend.core.domain.user.mysql")
 public class BlogCoreApplication {
 
     public static void main(String[] args) {
