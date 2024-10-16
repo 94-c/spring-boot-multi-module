a355f81f8e5f0b4a0d13ea44e64d805b665cf62e
94-c <hyeongwoo26@gmail.com>
2024-10-16 09:28:53 +0900

    [feat] api 모듈 jpa 추가

--- blog-api/src/main/java/com/backend/api/BlogApiApplication.java
+++ blog-api/src/main/java/com/backend/api/BlogApiApplication.java
@@ -2,12 +2,9 @@ package com.backend.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class})
+@SpringBootApplication
 @ComponentScan(basePackages = {"com.backend.api", "com.backend.core"})
 public class BlogApiApplication {
 
