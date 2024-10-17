bf3cb62470d63b5d5c10991dfe48a4a39a28013e
94-c <hyeongwoo26@gmail.com>
2024-10-17 17:26:14 +0900

    [remove] 회원가입 Valid 삭제

--- blog-api/src/main/java/com/backend/api/domain/auth/dto/request/CreateUserRequest.java
+++ blog-api/src/main/java/com/backend/api/domain/auth/dto/request/CreateUserRequest.java
@@ -16,19 +16,10 @@ import java.util.Set;
 @Getter @Setter
 public class CreateUserRequest {
 
-    @Email(message = "유효한 이메일 주소를 입력해 주세요.")
-    @NotBlank(message = "이메일은 필수 입력 사항입니다.")
     private String email;
-
-    @NotBlank(message = "닉네임은 필수 입력 사항입니다.")
     private String nickname;
-
-    @NotNull(message = "성별을 입력해 주세요.")
     private UserGenderType gender;
-
-    @NotNull(message = "소셜 타입을 입력해 주세요.")
     private UserSocialType social;
-
     private Double weight;
     private Double height;
     private Set<Role> roles;
@@ -43,16 +34,5 @@ public class CreateUserRequest {
         this.roles = roles;
     }
 
-    public static UserCreateData of(CreateUserRequest request) {
-       return new UserCreateData(
-               request.getEmail(),
-               request.getNickname(),
-               request.getGender(),
-               request.getSocial(),
-               request.getWeight(),
-               request.getHeight(),
-               request.getRoles()
-       );
-    }
 
 }
